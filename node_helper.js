9057b4b20b7e7d0fc7e02d333c914359328fb0c1
Captsi <cowdell@hotmail.com>
2021-03-27 08:10:25 +0000

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,63 +1,49 @@
 /* global module */
 
 /* Magic Mirror
- * Node Helper: MMM-COVID19
+ * Node Helper: MMM-TidesUK
  *
- * By Jose Forte
+ * By Simon Cowdell
  * MIT Licensed.
+ * byCountryUrl
  */
 
 var NodeHelper = require('node_helper')
 var request = require('request')
 
-var byCountryUrl = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php'
-var worldStatsUrl = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php'
+			url: "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0068/TidalEvents?duration=1", 
 
 module.exports = NodeHelper.create({
+        
   start: function () {
     console.log('Starting node helper for: ' + this.name)
   },
-  getGlobalStats: function(key) {
+  getTides: function(key) {
     var self = this
     var options = {
       method: 'GET',
-      url: worldStatsUrl,
+      url: url,
+//        'x-rapidapi-key': key
       headers: {
-        'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
-        'x-rapidapi-key': key
-      }
+			  'Host': 'admiraltyapi.azure-api.net',
+			  'Ocp-Apim-Subscription-Key':'515a75514aee4a07837db6d60bc10e41',
+		    'Content-Type': 'application/json'
+		    		}
+      
     }
     request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var result = JSON.parse(body)
-        self.sendSocketNotification('GLOBAL_RESULT', result)
+        console.log(response.statusCode + result);    		// uncomment to see in terminal
+         self.sendSocketNotification('TIDAL_RESULT', result)
       }
     })
   },
-  getStatsByCoutry: function(key) {
-    var self = this
-    var options = {
-      method: 'GET',
-      url: byCountryUrl,
-      headers: {
-        'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
-        'x-rapidapi-key': key
-      }
-    }
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var result = JSON.parse(body)
-        self.sendSocketNotification('BYCOUNTRY_RESULT', result)
-      }
-    })
-  },
+
   //Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
-    if (notification === 'GET_BY_COUNTRY_STATS') {
-      this.getStatsByCoutry(payload)
-    }
-    if (notification === 'GET_GLOBAL_STATS') {
-      this.getGlobalStats(payload)
+    if (notification === 'GET_TIDES') {
+      this.getTides(payload)
     }
   }
   
