c1651883173df529a2369693864881daca7408ce
Captsi <cowdell@hotmail.com>
2024-11-20 23:13:35 +0000

    Update node_helper.js
    Rewrite to remove references to request and replace with fetch

--- node_helper.js
+++ node_helper.js
@@ -6,49 +6,37 @@
  * By Simon Cowdell
  * MIT Licensed.
  * byCountryUrl
+ * Rewrite and simplification to avoid request.
  */
 
 var NodeHelper = require('node_helper')
-var request = require('request')
-
-//			url: "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0068/TidalEvents?duration=1", 
-//
 module.exports = NodeHelper.create({
-      start: function () {
-        console.log('Starting node helper for: ' + this.name)
-  },
-  getTides: function() {
-    var self = this;
-    var myurl= "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/" + this.config.StationID + "/TidalEvents?duration=" + this.config.Duration ;
-    
-  //  "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0068/TidalEvents?duration=3"
-    var options = {
-      method: 'GET',
-      url: myurl,
-//      https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0068/TidalEvents?duration=3",
-//        'x-rapidapi-key': key
-      headers: {
-			  'Host': 'admiraltyapi.azure-api.net',
-			  'Ocp-Apim-Subscription-Key': this.config.AdmiraltyKey,
-		    'Content-Type': 'application/json'
-		    		}
-      
-    }
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var result = JSON.parse(body)
-  //      console.log(response.statusCode + result);    		// uncomment to see in terminal
-         self.sendSocketNotification('TIDAL_RESULT', result)
-      }
-    })
-  },
 
-  //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'GET_TIDES') {
-      this.config = payload
-      this.getTides()
+  async socketNotificationReceived(notification, payload) {
+    const self = this
+
+    if (notification === "GET_TIDES") {
+      let url = "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/" + payload.StationID + "/TidalEvents?duration=3";
+
+      if (payload.difficulties)
+        url += `&difficulties=${payload.difficulties}`
+      if (payload.tags)
+        url += `&tags=${payload.tags}`
+
+      try {
+        const response = await fetch(url, {
+          headers: {
+          'Host': 'admiraltyapi.azure-api.net',
+          'Ocp-Apim-Subscription-Key': payload.AdmiraltyKey,
+          'Content-Type': 'application/json'
+             }})
+         const body = await response.json();
+        self.sendSocketNotification('TIDAL_RESULT', body)
+      } catch (error) {
+        self.sendSocketNotification("TIDES_FAILED", error);
+      }
     }
-  }
+  },
+})
   
 });
